62656d8e16b91af6cd3d6c1fc5472d5e3d035ac9
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2026-04-10 18:03:02 -0600

    Corrected an issue with translations that include Inner html in the text

--- script.js
+++ script.js
@@ -264,6 +264,6 @@ function setLanguage(lang) {
 
     document.querySelectorAll("[data-i18n]").forEach(el => {
         const key = el.getAttribute("data-i18n");
-        el.textContent = translations[lang][key];
+        el.innerHTML = translations[lang][key];
     });
 }
